42fb3d8dc7d4d51613533a95315525ff937252a5
Cabdinasir64 <cabdinasira64@gmail.com>
2025-08-30 13:35:45 +0300

    This is example1 of Variables of Typescript

---
+++ Variables/example1.ts
@@ -0,0 +1,11 @@
+// Variables
+let age: number = 32
+let Myname: string = "Mohamed"
+let IsStudent: boolean = true
+const Pi: number = 3.14
+let FavoriteCars: string[] = ["Ferrari"]
+
+console.log("My name is " + Myname + " and I am " + age + " years old.")
+console.log("Am I a student? " + IsStudent)
+console.log("The value of Pi is " + Pi)
+console.log("My favorite cars are: " + FavoriteCars.join(", "))
